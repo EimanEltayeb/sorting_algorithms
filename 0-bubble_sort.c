707a4b74dba94e2dd5d216588a9949dc16b52512
EimanEltayeb <em_moh092@hotmail.com>
2024-01-22 02:07:42 +0300

    d

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,18 +8,19 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	int x;
+	int x = 0;
 	size_t i, j;
 
 	for (j = 0; j < (size - 1); j++)
 	{
-		for (i = j; i < size; i++)
+		for (i = 0; i < (size - 1); i++)
 		{
 			if (array[i] > array[i + 1])
 			{
 				x = array[i + 1];
 				array[i + 1] = array[i];
 				array[i] = x;
+				print_array(array, size);
 			}
 		}
 	}
