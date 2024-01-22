4cececbf30aa7791d0cd682b67bdfc0fcdcd7be7
EimanEltayeb <em_moh092@hotmail.com>
2024-01-23 00:02:50 +0300

    d

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,9 +1,10 @@
 #include "sort.h"
 /**
- * insertion_sort_list - sort doubly linked list with insertion
- * @list: linked list to be sorted
+ * swap - swaps two nodes
+ * @t1: first node
+ * @t2: second node
 */
-/*void swap(listint_t **t1, listint_t**t2)
+void swap(listint_t **t1, listint_t **t2)
 {
 	listint_t *s1 = *t1, *s2 = *t2;
 
@@ -16,27 +17,46 @@
 	s2->prev = s1->prev;
 	s1->prev = s2;
 }
+/**
+ * insertion_sort_list - sort doubly linked list with insertion
+ * @list: linked list to be sorted
 */
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *t1, *t2, *tmp;
+	int a = 1;
 
+	if (*list == NULL || (*list)->next == NULL)
+		return;
 	t1 = *list;
 	t2 = (*list)->next;
-	tmp = t2;
-	while (t1->next != NULL)
+	tmp = t2->next;
+	while (t2)
 	{
-		while (t1->prev != NULL)
+		while (t1)
 		{
-			if (t1->n > t1->n)
+			if (t1->n > t2->n)
 			{
 				swap(&t1, &t2);
-				
+				t1 = t2->prev;
+				if (t1 == NULL)
+					*list = t2;
+				print_list(*list);
+				continue;
 			}
 			else
-			{
-
-			}
+				break;
 		}
+		if (a == 1)
+		{
+		t2 = tmp;
+		t1 = t2->prev;
+		if (t2->next != NULL)
+			tmp = t2->next;
+		else
+			a = 0;
+		}
+		else
+			break;
 	}
 }
