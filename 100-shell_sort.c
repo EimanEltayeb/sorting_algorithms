6fe1e6e47603d7eaf9e89a74dfb82a1c059a87ce
EimanEltayeb <em_moh092@hotmail.com>
2024-01-24 01:59:08 +0300

    100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,15 +1,26 @@
 #include "sort.h"
+/**
+ * swap - swaps two array elements
+ * @a1: first element
+ * @a2: second element
+*/
 
+void swap(int *a1, int *a2)
+{
+	int x;
+
+	x = *a1;
+	*a1 = *a2;
+	*a2 = x;
+}
 /**
  * shell_sort - sort using shell method
  * @array: array to be sorted
  * @size: size of the array
 */
-
 void shell_sort(int *array, size_t size)
 {
-	size_t i, gap, c, sgap;
-	int x;
+	size_t i, gap, c;
 
 	if (size < 1)
 		return;
@@ -17,49 +28,32 @@ void shell_sort(int *array, size_t size)
 	{
 		if (gap * 3 + 1 > size)
 			break;
-		continue;
-	}
-	sgap = gap;
-
-
-
-
-
+		continue; }
 	for (; gap >= 1; gap = (gap - 1) / 3)
 	{
-		printf("gap changed\n");
 		for (i = 0; (i + gap) < size; i++)
 		{
-			printf("i changed\n");
 			c = i;
 			if (array[i] > array[i + gap])
-				{
-					x = array[i];
-					array[i] = array[i + gap];
-					array[i + gap] = x;
-				}
-			if (i >= gap)
 			{
-				i = i - gap;
-				while (i >= gap)
+				swap(&array[i], &array[i + gap]);
+				if (i >= gap)
 				{
-					if (array[i] > array[i + gap])
+					while (i >= gap)
 					{
-						x = array[i];
-						array[i] = array[i + gap];
-						array[i + gap] = x;
-						i = i - gap;
-					}
-					else
-					{	
-						i = c;
-						break;
+						if (array[i] < array[i - gap])
+						{
+							swap(&array[i], &array[i - gap]);
+							i = i - gap;
+						}
+						else
+						{
+							i = c;
+							break; }
 					}
 				}
 			}
 			i = c;
 		}
-		if (gap < sgap)
-			print_array(array, size);
-	}
+		print_array(array, size); }
 }
