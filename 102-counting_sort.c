fd434da7db0c8c5af58e2c74d57e309e3832542f
EimanEltayeb <em_moh092@hotmail.com>
2024-01-24 02:54:39 +0300

    d

---
+++ 102-counting_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/**
+ * counting_sort - sorting array
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void counting_sort(int *array, size_t size)
+{
+	if (size <= 1)
+		return;
+	print_array(array, size);
+}
