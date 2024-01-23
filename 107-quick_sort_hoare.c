ec9e8b94ccd1f4761e452533a8b5a51505aa84de
EimanEltayeb <em_moh092@hotmail.com>
2024-01-24 02:47:21 +0300

    d

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ * quick_sort_hoare - sorting array
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (size <= 1)
+		return;
+}
