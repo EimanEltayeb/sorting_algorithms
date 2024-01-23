2af0c0ed7d1f51a285c21b42bd25a46e71442ec9
EimanEltayeb <em_moh092@hotmail.com>
2024-01-24 01:30:02 +0300

    d

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,5 +8,58 @@
 
 void shell_sort(int *array, size_t size)
 {
-	
+	size_t i, gap, c, sgap;
+	int x;
+
+	if (size < 1)
+		return;
+	for (gap = 1; gap <= size; gap = gap * 3 + 1)
+	{
+		if (gap * 3 + 1 > size)
+			break;
+		continue;
+	}
+	sgap = gap;
+
+
+
+
+
+	for (; gap >= 1; gap = (gap - 1) / 3)
+	{
+		printf("gap changed\n");
+		for (i = 0; (i + gap) < size; i++)
+		{
+			printf("i changed\n");
+			c = i;
+			if (array[i] > array[i + gap])
+				{
+					x = array[i];
+					array[i] = array[i + gap];
+					array[i + gap] = x;
+				}
+			if (i >= gap)
+			{
+				i = i - gap;
+				while (i >= gap)
+				{
+					if (array[i] > array[i + gap])
+					{
+						x = array[i];
+						array[i] = array[i + gap];
+						array[i + gap] = x;
+						i = i - gap;
+					}
+					else
+					{	
+						i = c;
+						break;
+					}
+				}
+			}
+			i = c;
+		}
+		if (gap < sgap)
+			print_array(array, size);
+	}
 }
