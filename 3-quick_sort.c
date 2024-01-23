883c10cbcb9bfafa8c74070cf597192cf826af01
EimanEltayeb <em_moh092@hotmail.com>
2024-01-23 04:21:25 +0300

    d

---
+++ 3-quick_sort.c
@@ -0,0 +1,62 @@
+#include "sort.h"
+size_t partition(int *array, size_t low, size_t high, size_t size);
+/**
+ * sort - sort the elements
+ * @array: the array to be sorted
+ * @high: num
+ * @low: num
+ * @size: array size
+*/
+void sort(int *array, size_t low, size_t high, size_t size)
+{
+	size_t p;
+
+	if (low >= high)
+		return;
+	p = partition(array, low, high, size);
+	if (p != 0)
+		sort(array, low, p - 1, size);
+	sort(array, p + 1, high, size);
+}
+/**
+ * partition - a function
+ * @array: the array to be sorted
+ * @high: num
+ * @low: num
+ * @size: array size
+ * Return: postition
+*/
+size_t partition(int *array, size_t low, size_t high, size_t size)
+{
+	int pivot = array[high], x;
+	size_t i, j;
+
+	i = low - 1; /*temp p index*/
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			x = array[i];
+			array[i] = array[j];
+			array[j] = x;
+		}
+	}
+	x = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = x;
+	print_array(array, size);
+	return (i + 1);
+}
+
+/**
+ * quick_sort - sorts an array using Quick Sort method
+ * @array: the array to be sorted
+ * @size: number of elements of the array
+*/
+
+void quick_sort(int *array, size_t size)
+{
+	if (size > 1)
+		sort(array, 0, size - 1, size);
+}
