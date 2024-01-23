34d79f08aaa576e65880cce1d48b68b211b07d06
EimanEltayeb <em_moh092@hotmail.com>
2024-01-23 05:29:05 +0300

    d

---
+++ 100-shell_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sort using shell method
+ * @array: array to be sorted
+ * @size: size of the array
+*/
+
+void shell_sort(int *array, size_t size)
+{
+	
+}
